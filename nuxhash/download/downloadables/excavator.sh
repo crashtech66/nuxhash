03e20229badf8202bde0dd509c8d64ed07ec92ab
crashtech66 <86744402+crashtech66@users.noreply.github.com>
2021-11-16 19:52:21 -0700

    Update excavator.sh
    Just hacking around trying to make an updated miner work.

--- nuxhash/download/downloadables/excavator.sh
+++ nuxhash/download/downloadables/excavator.sh
@@ -21,23 +21,44 @@
      
 # BEGIN REVISED VERSION:    
      
-  VERSION='1.7.1d'
-  SHA256='2cf36f8dfa54bcbc4658e4205dd63db35cf0f1c228e07c8818269825a75699bc'
+#  VERSION='1.7.1d'
+#  SHA256='2cf36f8dfa54bcbc4658e4205dd63db35cf0f1c228e07c8818269825a75699bc'
+# case "$1" in
+# verify)
+#       [ -f excavator ] && [ `sha256sum excavator | awk '{print $1}'` = "$SHA256" ]
+#        exit $?
+#        ;;
+#download)
+     #   curl -L -O "https://github.com/nicehash/excavator/releases/download/v${VERSION}/excavator_${VERSION}_amd64.deb"
+     #   url taken from github link to file: https://github.com/nicehash/excavator/archive/refs/tags/v1.7.1d.tar.gz
+#         curl -L -O "https://github.com/nicehash/excavator/archive/refs/tags/v${VERSION}.tar.gz"
+     #  https://github.com/nicehash/excavator/archive/refs/tags/v1.7.1d.tar.gz
+     #  ar x "excavator_${VERSION}_amd64.deb" data.tar.xz
+#        mv v${VERSION}.tar.gz data.tar.xz
+#        tar xf data.tar.xz --strip-components 4 ./opt/excavator/bin/excavator
+     #  rm -f "excavator_${VERSION}_amd64.deb" data.tar.xz
+#        rm -f "v${VERSION}.tar.gz" data.tar.xz
+#        exit 0
+#        ;;
+#esac
+
+#!/bin/sh
+
+# Revision 2:
+     
+     VERSION='1.7.1d'
+SHA256='2cf36f8dfa54bcbc4658e4205dd63db35cf0f1c228e07c8818269825a75699bc'
+
 case "$1" in
 verify)
         [ -f excavator ] && [ `sha256sum excavator | awk '{print $1}'` = "$SHA256" ]
         exit $?
         ;;
 download)
-     #   curl -L -O "https://github.com/nicehash/excavator/releases/download/v${VERSION}/excavator_${VERSION}_amd64.deb"
-     #   url taken from github link to file: https://github.com/nicehash/excavator/archive/refs/tags/v1.7.1d.tar.gz
-         curl -L -O "https://github.com/nicehash/excavator/archive/refs/tags/v${VERSION}.tar.gz"
-     #  https://github.com/nicehash/excavator/archive/refs/tags/v1.7.1d.tar.gz
-     #  ar x "excavator_${VERSION}_amd64.deb" data.tar.xz
-        mv v${VERSION}.tar.gz data.tar.xz
+        curl -L -O "https://github.com/nicehash/excavator/releases/download/v${VERSION}/excavator_${VERSION}_amd64.deb"
+        ar x "excavator_${VERSION}_amd64.deb" data.tar.xz
         tar xf data.tar.xz --strip-components 4 ./opt/excavator/bin/excavator
-     #  rm -f "excavator_${VERSION}_amd64.deb" data.tar.xz
-        rm -f "v${VERSION}.tar.gz" data.tar.xz
+        rm -f "excavator_${VERSION}_amd64.deb" data.tar.xz
         exit 0
         ;;
 esac
